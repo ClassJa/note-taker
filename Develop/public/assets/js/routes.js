eaef491501bb636f0ee6247ab9844b39cfde46ba
JA <69608172+ClassJay@users.noreply.github.com>
2024-07-15 16:11:36 -0500

    Added routes

--- Develop/public/assets/js/routes.js
+++ Develop/public/assets/js/routes.js
@@ -8,15 +8,40 @@ const db = require('../../../db/db.json')
 
 // const notes = require('../../../../Develop/')
 
-app.get('/', (res, req) => {
-    res.send(db)
 
+app.get('*', (res, req) => {
+    res.sendFile('index.html')
+})
+
+
+app.get('/notes', (res, req) => {
+    res.sendFile('notes.html')
+})
+
+
+app.get('/api/notes', (res, req) => {
+    res.json(db)
+})
+
+app.post('/api/notes', (res, req) => {
+    res.json()
+
+    // look into npm packages that can give the notes unique ids uuid
+    // https://www.npmjs.com/package/generate-unique-id
+})
+
+
+// this 
+app.get('/', (res, req) => {
+    res.send('Navigate to a /notes or / route')
+})
+// or this
+
+app.get('/api/notes', (res, req) => {
+    res.sendFile('index.html')
 })
 
 app.get('/db', () => {
     res.json(db)
 })
 
-app.get('/', (res, req) => {
-    res.send()
-})
